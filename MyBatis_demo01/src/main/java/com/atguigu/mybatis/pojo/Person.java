442b55f7c2413d09a27e75cf075c2d1e1fc78454
txy <1965768655@qq.com>
2022-12-05 19:19:54 +0800

    third commit

---
+++ MyBatis_demo01/src/main/java/com/atguigu/mybatis/pojo/Person.java
@@ -0,0 +1,34 @@
+package com.atguigu.mybatis.pojo;
+
+/**
+ * @author txy
+ * @create 2022-12-05 19:16
+ */
+public class Person {
+    String name;
+    int age;
+
+    @Override
+    public String toString() {
+        return "Person{" + "name='" + name + '\'' + ", age=" + age + '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person() {
+    }
+}
